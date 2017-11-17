04afb6528e0f5d03e01fcf32548c418bfd9188e0
Lars De Richter <lars.derichter@gmail.com>
2017-11-17 01:30:18 +0100

    Initial commands for post-install on macos

---
+++ macos-postinstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make my preferred directories if they do not exist yet
+mkdir -p ~/Documents/Administration
+mkdir -p ~/Documents/Projects
+mkdir -p ~/Documents/temp
+mkdir -p ~/Documents/Websites
+mkdir -p ~/Documents/Writing
+
+# Install XCode command line tools
+xcode-select --install
